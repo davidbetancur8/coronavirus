d2292e4eb5cb170f26a1b4a6bfe013f1d4444135
davidbetancur8 <david.bet.san@gmail.com>
2020-03-24 18:52:18 -0500

    added colombia to the dashboard

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ import pandas as pd
 import plotly.express as px
 import io
 import requests
-
+from urllib.request import urlopen
+import json
 
 def load_dataset(tipo):
     if tipo == "Confirmed":
@@ -86,6 +87,48 @@ def generar_casos(tipo):
                  )
     return fig
 
+def genrar_cuenta_colombia():
+    lista = ["Amazonas","Antioquia","Arauca","Atlántico","Bogotá","Bolívar",
+    "Boyacá","Caldas","Caquetá","Casanare","Cauca","Cesar","Chocó",
+    "Córdoba","Cundinamarca","Guainía","Guaviare","Huila","La Guajira","Magdalena",
+    "Meta","Nariño","Norte de Santander","Putumayo","Quindío","Risaralda","San Andrés y Providencia",
+    "Santander","Sucre","Tolima","Valle del Cauca","Vaupés","Vichada"]
+    lista = [depto.upper() for depto in lista]
+    ceros = [0]*len(lista)
+    df_ceros = pd.DataFrame({"NOMBRE_DPT":lista, "cuenta_ceros":ceros})
+
+    df_data = pd.read_csv("data/Casos1.csv")
+    df_data = df_data.rename(columns={"Departamento": "NOMBRE_DPT"})
+    df_data["NOMBRE_DPT"] = df_data["NOMBRE_DPT"].str.upper()
+    df_cuenta = pd.DataFrame(df_data.groupby("NOMBRE_DPT")["ID de caso"].count()).reset_index().rename(columns={"ID de caso": "cuenta"})
+    df_merge = df_ceros.merge(df_cuenta, on="NOMBRE_DPT", how="left")
+    df_merge["total"] = df_merge["cuenta"] + df_merge["cuenta_ceros"]
+    df_merge = df_merge.drop(["cuenta", "cuenta_ceros"], axis=1)
+    nombres_dict = {"BOGOTÁ": "SANTAFE DE BOGOTA D.C",
+                    "VALLE": "VALLE DEL CAUCA"}
+    for dept in nombres_dict:
+        df_merge = df_merge.replace(dept, nombres_dict[dept])
+    df_merge = df_merge.fillna(0)
+    df_merge['NOMBRE_DPT'] = df_merge['NOMBRE_DPT'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    df_merge = df_merge.replace("NARINO", "NARIÑO")
+    return df_merge
+
+def generar_mapa_colombia_cuenta():
+    df_merge = genrar_cuenta_colombia()
+    with urlopen('https://gist.githubusercontent.com/john-guerra/43c7656821069d00dcbc/raw/be6a6e239cd5b5b803c6e7c2ec405b793a9064dd/Colombia.geo.json') as response:
+        counties = json.load(response)
+
+    fig = px.choropleth(df_merge, geojson=counties, color="total",
+                        locations="NOMBRE_DPT", featureidkey="properties.NOMBRE_DPT",
+                        projection="mercator",scope="south america",color_continuous_scale=px.colors.sequential.Reds
+                    )
+    fig.update_geos(fitbounds="locations", visible=False, showcountries=True, countrycolor="Black",
+        showsubunits=True)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+
+
 
 df_confirmed = load_dataset("Confirmed")
 df_recovered = load_dataset("Recovered")
@@ -122,7 +165,14 @@ app.layout = dbc.Container([
 
     ),
     
-    html.Div([
+    html.Div([  dbc.Row([
+                    html.Div(
+                        dcc.Graph(
+                            id = "mapa_colombia",
+                            figure = generar_mapa_colombia_cuenta()
+                        ), className="pretty_container"
+                    ),
+                ]),
                 dbc.Row([
                     html.Div(
                         dcc.RadioItems( 
